89c54001550ada20b8f58056fadb04cb24bb9374
vfine <vfine@finecomputing.com>
2018-10-28 17:47:56 -0400

    Adding  'Under Consstruction"

--- inter.php
+++ inter.php
@@ -18,9 +18,7 @@ script type="text/javascript" src="Rutenberg%20%C2%B7%20The%20Smart%20Brokers_fi
 <meta name="keywords" content="Midtown, Midtown Manhattan, Chelsea,Hudson Yards,Hudson,Yards,Central, park,lower">
 <meta name="keywords" content="Central, park,NOLO, development,for sale, residential, properties, investment, sale, buy, foreclosures,public">
 <title>
-
   Private NYC OMRE Advisors
-
 </title>
 </head>
 <body class="home">
@@ -32,7 +30,7 @@ script type="text/javascript" src="Rutenberg%20%C2%B7%20The%20Smart%20Brokers_fi
 </div>
 
 <div class="center">
-	<h2>Privacy Policy</h2>
+	<h2>International</h2>
 </div>
 
 <div class="main">
@@ -41,36 +39,9 @@ At [This company name], we are committed to protecting your privacy and creating
 By visiting [this website url], you are accepting the practices described in this Privacy Notice.
 </p>
 
-<h3>The Personal Information Collected and How it is used</h3>
+<h3>International</h3>
 
-<p>
-We collect and use various types of information to service your accounts, save you time and money, and better respond to your needs.
-<br/>
-When you submit information to [this website url] for a loan or realty request, you authorize us to share this information to facilitate the services you request by matching you with our network of participating Lenders, real estate professionals or other parties involved in your transaction. A user must provide detailed information about each loan or realty request such as your name, phone number, e-mail address, property address, and price range. By providing this information to us, or by submitting a request, you expressly consent to being contacted by us and by our lenders and real estate agents, or service providers via phone, email, mail or other reasonable means, at any of your contact numbers or addresses, in order that we may provide the services set forth on our Web site or for other reasonable purposes related to our business.
-<br/>
-</p>
-<p>
-During our process, users are required to give the company contact information such as their name, email address, home address, & phone number. The company uses this information to solely to help match you with up to 3 top prescreened agents.
-</p>
-<h3>What We Won’t Do With Personal Information</h3>
-<p>We will share your personal information with third parties only in the ways that are described in this privacy policy. We promise never to sell or rent any of your personal information without your prior consent. Your trust and confidence are our highest priority. For questions or concerns regarding our privacy policy, please contact [this website url]
-Customer Support. </p>
-<h3>Legal Disclaimer</h3>
-<p>
-The company reserves the right to disclose users personal information as required by law and when company believe that disclosure is necessary to protect the company rights and/or to comply with a judicial proceeding, court order, or legal process served on the company. The company follows generally accepted industry standards to protect the personal information submitted to the company, both during transmission and once the company receives it. No method of transmission over the Internet, or method of electronic storage, is 100% secure, however. Therefore, while the company strives to use commercially acceptable means to protect user’s personal information, the company cannot guarantee its absolute security. If user shave any questions about security on the company Web site, user can send email company at
-[support email]
-</p>
-<h3> Access to Personal Information</h3>
-<p> If your personal information changes, or if you no longer desire our service, you may correct, update, delete, or deactivate it by making  by emailing our Customer Support at
-[email]or by contacting us by telephone or postal mail at the contact information listed below.
-<br/>
-We will retain your information for as long as your account is active or as needed to provide you services. We will retain and use your information as necessary to comply with our legal obligations, resolve disputes, and enforce our agreements.<p>
-</div>
-<h3> Terms of Use </h3>
-<p> By accessing, browsing, and using this Site, you acknowledge that you have read, understood and agree to comply with the terms and conditions stated here:
-Terms [URL to Terms of use Page].<br/>
-If you do not agree to be bound by the terms and conditions of this Agreement, do not use or access our services. These Terms of Use are subject to change at any time, without prior notice.
-</p>
+<h4> Under Construction! </h4>
 
 <ul class="bottomnav">
   <li><a class="active" href="http://consulting.finecomputing.com">Home</a>
