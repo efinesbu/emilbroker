ffade3a870346b3318a4ed87ee4e2e7f999ea22e
vfine <vfine@finecomputing.com>
2018-11-04 21:02:42 -0500

    Intoducing privacypolicy.php

--- site_top_menu.php
+++ site_top_menu.php
@@ -1,7 +1,7 @@
 <?php
 print <<<END
 <span>
-  <a class="conf" href="privacypolicy.html" title="See our privacy policy">Confidentiality</a> |
+  <a class="conf" href="privacypolicy.php" title="See our privacy policy">Confidentiality</a> |
   <a href="selection.php" title="See our selection">Catered Selection</a> |
   <a href="experience.php" title="our adviser are &gt;20years here">20+ Years Experience</a> |
   <a href="inter.php" title="Oversea clients are welcome">International</a>
