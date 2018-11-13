b667c8360678705fc5686f51955f83e4dfec4c2e
vfine <vfine@finecomputing.com>
2018-11-12 23:12:09 -0500

    cleanup tags

--- inter.php
+++ inter.php
@@ -38,8 +38,8 @@ script type="text/javascript" src="Rutenberg%20%C2%B7%20The%20Smart%20Brokers_fi
 </div>
 <ul class="bottomnav">
   <li><a class="active" href="http://consulting.finecomputing.com">Home</a>
-  </li><li><a href="contact.php">Contact Us</a>
-	</li><li><a href="about.php">About</a>
-</li></ul>
+  <li><a href="contact.php">Contact Us</a>
+	<li><a href="about.php">About</a>
+</ul>
 <?php include 'site_footer.php'; ?>
 </html>
