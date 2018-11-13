cf8f5b5a0ca3a61b0f573512a3acecce5bf22cb7
vfine <vfine@finecomputing.com>
2018-11-12 23:11:08 -0500

    adding yahoo logo

--- site_footer.php
+++ site_footer.php
@@ -1,6 +1,9 @@
 <div class="main">
 <?php
 print "<hr>";
+echo '<a href="https://www.yahoosmallbusiness.com/webhosting/" target="_top">';
+echo '<img src="https://s.yimg.com/lm/i/us/smallbiz/gr/hostedby.gif" width="118" height="45" border="0" align="middle" alt="Hosting by Yahoo Small Business">';
+echo "</a> ";
 print "<tiny><code>";
 print date(DATE_RFC2822);
 print " &copy;FineAssociates";
