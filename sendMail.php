da87bf4196e6e20a625001a123ffa5cfd0681576
vfine <vfine@finecomputing.com>
2018-11-25 23:11:03 -0500

    Replacing "echo" with "error_log".

--- sendMail.php
+++ sendMail.php
@@ -12,10 +12,10 @@ See: https://p10.secure.webhosting.luminate.com/phpmailsetup?ssc=us1
 
 <?php
 require 'lib.php';
-$subject = htmlentities($_POST["subject"]);
-$msg = htmlentities($_POST["msg"]);
-$firstname =  htmlentities($_POST["firstname"]);
-$lastname = htmlentities($_POST["lastname"]);
+$subject   = htmlentities($_POST["subject"]);
+$msg       = htmlentities($_POST["msg"]);
+$firstname = htmlentities($_POST["firstname"]);
+$lastname  = htmlentities($_POST["lastname"]);
 if ($lastname == ''  and $firstname == '') {
   $firstname = "Customer";
 }
@@ -33,19 +33,19 @@ if ($subject != '' and $msg!='') {
     "Role"     => "client",
     "home"     => "Poquotte"
   );
-  echo "test $user_attr<br>";
+  error_log("test $user_attr<br>");
   $location =  $_SERVER['REMOTE_ADDR'];
   $user_message = array (
     "message" => $msg,
     "subject" => $subject,
     "host"    => $_SERVER['REMOTE_ADDR']
   );
-  echo "server name: " .$user_message["host"] . ": $msg<br>";
   $user_id = initial_client_inquiry($user_attr, $user_message);
   $msg = "We have gotten a message: '$msg' from client: $firstname $lastname. The client was registered as #$user_id from $location location.";
-  echo "server name" . $user_id ." " . $msg . "<br>";
   $email = get_list_of_admin_email_addr();
-  mail($email, "Test message from $location: " . $subject, $msg);
+  if ($firstname != 'test') {
+    mail($email, "Test message from $location: " . $subject, $msg);
+  }
 
   /*
   * NOTE: PHP caches file status so we need to clear
@@ -63,7 +63,7 @@ if ($subject != '' and $msg!='') {
     $dt = date(DATE_RFC2822);
     print "Dear $firstname $lastname! <br>";
     print "On $dt we received your [$subject] request.  <br>";
-    print "Your message has been registred as <a href='contact.php?user_id=$user_id'>#$user_id</a> and forwarded to FineAssociates, LLC to see how can we help you";
+    print "Your message has been registered as <a href='contact.php?user_id=$user_id'>#$user_id</a> and forwarded to FineAssociates, LLC to see how can we help you";
     print "<p>You may check your request status here: <a href='contact.php?user_id=$user_id'>contact.php?user_id=$user_id'</a>. <p> Please, bookmark, and use it to track our progress.";
     print "<hr>Truly yours, FineAssociates.  <br>";
   }
