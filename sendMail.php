cf73114af188f4309119b57bccaee02c46d75d1d
vfine <vfine@finecomputing.com>
2018-11-25 22:08:08 -0500

    Notify aan user about its registartion and how to check the request status.

--- sendMail.php
+++ sendMail.php
@@ -11,9 +11,7 @@ See: https://p10.secure.webhosting.luminate.com/phpmailsetup?ssc=us1
 <body>
 
 <?php
-//$email = "fine@finecomputing.com;efinesbu@gmail.com;gpanasenko@moseco.com";
 require 'lib.php';
-$email = "fine@finecomputing.com;efinesbu@gmail.com";
 $subject = htmlentities($_POST["subject"]);
 $msg = htmlentities($_POST["msg"]);
 $firstname =  htmlentities($_POST["firstname"]);
@@ -33,20 +31,21 @@ if ($subject != '' and $msg!='') {
     "FirstName" => $firstname,
     "LastName" => $lastname,
     "Role"     => "client",
-    "home" => "Poquotte"
+    "home"     => "Poquotte"
   );
   echo "test $user_attr<br>";
+  $location =  $_SERVER['REMOTE_ADDR'];
   $user_message = array (
     "message" => $msg,
     "subject" => $subject,
     "host"    => $_SERVER['REMOTE_ADDR']
   );
   echo "server name: " .$user_message["host"] . ": $msg<br>";
-
   $user_id = initial_client_inquiry($user_attr, $user_message);
-  $msg = "$msg<br> user_id registred as $user_id";
+  $msg = "We have gotten a message: '$msg' from client: $firstname $lastname. The client was registered as #$user_id from $location location.";
   echo "server name" . $user_id ." " . $msg . "<br>";
-  mail($email, "Test message" . $subject, $msg);
+  $email = get_list_of_admin_email_addr();
+  mail($email, "Test message from $location: " . $subject, $msg);
 
   /*
   * NOTE: PHP caches file status so we need to clear
@@ -64,11 +63,9 @@ if ($subject != '' and $msg!='') {
     $dt = date(DATE_RFC2822);
     print "Dear $firstname $lastname! <br>";
     print "On $dt we received your [$subject] request.  <br>";
-    print "Your message has been registred as <a href='contact.php?user_id=$user_id'>#$user_id</a> and forwarded to FineAssociates, LLC to see how can we help you<br>";
-    print "To check your request status here <a href='contact.php?user_id=$user_id'>contact.php?user_id=$user_id'</a>, please, bookmark, and use it to click to track our progress.";
-    print "Truly yours, FineAssociates.  <br>";
-    print "<br>";
-    print "$dt <br>";
+    print "Your message has been registred as <a href='contact.php?user_id=$user_id'>#$user_id</a> and forwarded to FineAssociates, LLC to see how can we help you";
+    print "<p>You may check your request status here: <a href='contact.php?user_id=$user_id'>contact.php?user_id=$user_id'</a>. <p> Please, bookmark, and use it to track our progress.";
+    print "<hr>Truly yours, FineAssociates.  <br>";
   }
 } else {
   include 'site_footer.php';
