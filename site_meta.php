0ebe63ec57d773892c4f861f9e585d4f95d88f6a
vfine <vfine@finecomputing.com>
2018-12-16 20:05:15 -0500

    Adding a complex keyword

--- site_meta.php
+++ site_meta.php
@@ -3,11 +3,11 @@ print <<<END
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
 <meta name="description"><meta name="author">
+<meta name="keywords" content="'off-market real estate new-york'>
 <meta name="keywords" content="real,estate,new-york,usa,united,states,america,manhattan,brooklyn,long island,Queens">
 <meta name="keywords" content="home,apartment,co-op,condo,listing,new,new listing,new development,development,">
 <meta name="keywords" content="property, market,upper,east,west,south,east,Upper East Side,Upper West side,Tribeca, FIDI,">
 <meta name="keywords" content="Midtown, Midtown Manhattan, Chelsea,Hudson Yards,Hudson,Yards,Central, park,lower">
 <meta name="keywords" content="Central, park,NOLO, development,for sale, residential, properties, investment, sale, buy, foreclosures,public">
-<script></script>
 END;
 ?>
