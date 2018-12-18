55c2a2c514b86bd61fbdb2f4755add308faf8477
vfine <vfine@finecomputing.com>
2018-12-18 00:03:59 -0500

    new version

--- privacypolicy.php
+++ privacypolicy.php
@@ -23,13 +23,12 @@
 
 <div class="center">
 	<h2>Privacy Policy</h2>
-  <h3>We paraphrased this from TopAgents.com. It should be much shorter and our own one </h3>
 </div>
 
 <div class="main">
 <p>
-At OMRE, we are committed to protecting your privacy and creating a safe and secure online environment for anyone who visits and uses on our site. All information is confidential and is never sold, rented, or otherwise used
-for any purpose other than to help us match you with a bank, lender or broker (whom we will refer to all these as “Lenders”) or real estate agents who can meet your needs. This notice describes our privacy policy.
+At &copy;FineAssociates ,we are committed to protecting your privacy and creating a safe and secure online environment for anyone who visits and uses on our site. All information is confidential and is never sold, rented, or otherwise used for any purpose other than to help us match you with an agent(s) who can meet your needs.
+<p>This notice describes our privacy policy.
 By visiting <a href="http://consulting.finecomputing.com">consulting.finecomputing.com</a>,
 you are accepting the practices described in this Privacy Notice.
 </p>
@@ -37,34 +36,39 @@ you are accepting the practices described in this Privacy Notice.
 <h3>The Personal Information Collected and How it is used</h3>
 
 <p>
-We collect and use various types of information to service your accounts,
-save you time and money, and better respond to your needs.
-<br/>
-When you submit information to OMRE for a loan or realty request,
-you authorize us to share this information to facilitate the services you request by matching you with our network of participating Lenders, real estate professionals or other parties involved in your transaction. A user must provide detailed information about each loan or realty request such as your name, phone number, e-mail address, property address, and price range. By providing this information to us, or by submitting a request, you expressly consent to being contacted by us and by our lenders and real estate agents, or service providers via phone, email, mail or other reasonable means, at any of your contact numbers or addresses, in order that we may provide the services set forth on our Web site or for other reasonable purposes related to our business.
-<br/>
-</p>
+We collect and use various types of information to service our relationships,
+save you time and money, and better respond to your needs
+
 <p>
-During our process, users are required to give the company contact information such as their name,
-email address, home address, & phone number. The company uses this information
-to solely to help match you with up to 3 top prescreened agents.
+During our process, our clients may be advised to give the company contact information such as their name,
+email address, home address, & phone number.
 </p>
 <h3>What We Won’t Do With Personal Information</h3>
-<p>We will share your personal information with third parties only in the ways that are described in this privacy policy. We promise never to sell or rent any of your personal information without your prior consent. Your trust and confidence are our highest priority. For questions or concerns regarding our privacy policy, please contact OMRE
-Customer Support. </p>
+<p>Your trust and confidence are our highest priority.
+  We will not share your personal information with third parties  in ways that are not described in this privacy policy.
+  We promise never to sell or rent any of your personal information.
+   ©FineAssociates sole role is to act as a communication medium.
+    Discussion topics are at the discretion of the clients and agents.
+    For questions or concerns regarding our privacy policy, please contact our <a href='contact.php'>Customer Support</a>. </p>
 <h3>Legal Disclaimer</h3>
 <p>
-The company reserves the right to disclose users personal information as required by law and when company believe that disclosure is necessary to protect the company rights and/or to comply with a judicial proceeding, court order, or legal process served on the company. The company follows generally accepted industry standards to protect the personal information submitted to the company, both during transmission and once the company receives it. No method of transmission over the Internet, or method of electronic storage, is 100% secure, however. Therefore, while the company strives to use commercially acceptable means to protect user’s personal information, the company cannot guarantee its absolute security. If user shave any questions about security on the company Web site,
-<a href="contact.php">contact us</a>.
+The company reserves the right to disclose users personal information as required by law and
+when we believe that disclosure is necessary to protect the company rights and/or to comply with a judicial proceeding,
+court order, or legal process served on the company. The company makes an effort to protect the personal information submitted to the company.
+<p>
+ No method of transmission over the Internet, or method of electronic storage, is 100% secure, however.
+ Therefore, while the company strives to use commercially acceptable means to protect user’s personal information,
+ the company cannot guarantee its absolute security.
+ If you have any questions about security on the company Web site, <a href="contact.php">contact us</a>.
 </p>
 <h3> Access to Personal Information</h3>
-<p> If your personal information changes, or if you no longer desire our service, you may correct, update, delete, or deactivate it by making  by <a href="contact.php">contacting us</a>
+<!--<p> If your personal information changes, or if you no longer desire our service, you may correct, update, delete, or deactivate it by making  by <a href="contact.php">contacting us</a>
 or by telephone or postal mail at the contact information listed below.
 <br/>
-We will retain your information for as long as your account is active or as needed to provide you services.
- We will retain and use your information as necessary to comply with our legal obligations,
- resolve disputes, and enforce our agreements.<p>
-</div>
+-->
+<p>We will retain and use your information as necessary to comply with our legal obligations, resolve disputes, and enforce our agreements.
+If you have any questions about access to personal information.<a href="contact.php">contact us</a>.
+
 <h3> Terms of Use </h3>
 <p> By accessing, browsing, and using this Site, you acknowledge that you have read, understood and agree to comply with the terms and conditions stated here:
 Terms.<br/>
@@ -78,5 +82,6 @@ These Terms of Use are subject to change at any time, without prior notice.
 	</li><li><a href="about.php">About</a>
 </li></ul>
 <?php include 'site_footer.php'; ?>
+
 </body>
 </html>
