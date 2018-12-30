f5d6ad1b58778c66ae2960445939d5d65e69c0cc
vfine <vfine@finecomputing.com>
2018-12-30 10:58:00 -0500

    Change anayltic ID

--- google_analytic.php
+++ google_analytic.php
@@ -1,12 +1,31 @@
 <!-- Global site tag (gtag.js) - Google Analytics -->
 <script async  type="text/javascript" src="https://www.googletagmanager.com/gtag/js?id=UA-53560525-3"></script>
-<script  type="text/javascript">
+<!--<script  type="text/javascript">
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
   gtag('config', 'UA-53560525-3');
 </script>
+-->
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-2911625-4"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-2911625-4');
+</script>
+
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-KC26K6H');</script>
+<!-- End Google Tag Manager -->
+
 <!--
 <script src='https://www.google.com/recaptcha/api.js?render=6Lckl3kUAAAAAI876a2sygzSh1ti-FkG4uySZ-mZ'></script>
 
@@ -18,4 +37,4 @@ grecaptcha.execute('6Lckl3kUAAAAAI876a2sygzSh1ti-FkG4uySZ-mZ', {action: 'action_
 });
 });
 </script>
---> 
+-->
