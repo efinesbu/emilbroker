b0d737c0aeb1c2e3b73be0063d6654c16fdfe302
vfine <vfine@finecomputing.com>
2018-12-30 10:59:53 -0500

    site meta

--- contact_admin.php
+++ contact_admin.php
@@ -11,14 +11,7 @@ script type="text/javascript" src="Rutenberg%20%C2%B7%20The%20Smart%20Brokers_fi
 <script type="text/javascript" src="Rutenberg%20%C2%B7%20The%20Smart%20Brokers_files/_ate.config_resp"></script>
 
 -->
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
-<meta name="description"><meta name="author">
-<meta name="keywords" content="real,estate,new-york,usa,united,states,america,manhattan,brooklyn,long island,Queens">
-<meta name="keywords" content="home,apartment,co-op,condo,listing,new,new listing,new development,development,">
-<meta name="keywords" content="property, market,upper,east,west,south,east,Upper East Side,Upper West side,Tribeca, FIDI,">
-<meta name="keywords" content="Midtown, Midtown Manhattan, Chelsea,Hudson Yards,Hudson,Yards,Central, park,lower">
-<meta name="keywords" content="Central, park,NOLO, development,for sale, residential, properties, investment, sale, buy, foreclosures,public">
+<?php  require 'site_meta.php'; ?>
 <title>New-York Off-Market Real Estate Advisors</title>
 </head>
 <body class="home">
