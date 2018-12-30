66960207cabb5e9a596bffe5cdf61d95dfa73763
vfine <vfine@finecomputing.com>
2018-12-30 11:00:07 -0500

    adding "fee" menu

--- site_top_menu.php
+++ site_top_menu.php
@@ -4,6 +4,7 @@ print <<<END
   <a class="conf" href="privacypolicy.php" title="See our privacy policy">Confidentiality</a> |
   <a href="selection.php" title="See our selection">Catered Selection</a> |
   <a href="experience.php" title="our adviser are &gt;20years here">20+ Years Experience</a> |
+  <a href="fee.php" title="Our service is free to the buyers">Our Fee</a> |
   <a href="inter.php" title="Oversea clients are welcome">International</a>
   </span>
   <p></p>
