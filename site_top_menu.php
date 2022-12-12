2157fb650725f7407b7f5b2b0a43153b3c6ae2b3
Valeri Fine <fine@finecomputing.com>
2022-12-11 22:12:38 -0500

    Update site_top_menu.php

--- site_top_menu.php
+++ site_top_menu.php
@@ -7,6 +7,6 @@ print <<<END
   <a href="fee.php" title="Our service is free to the buyers">Our Fee</a> |
   <a href="inter.php" title="Oversea clients are welcome">International</a>
   </span>
-  <p></p>
+  <p></p> 
 END;
 ?>
